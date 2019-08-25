9395e92c3f26751ca4db011984447a59c61c29c2
Richard Petri <git@rpls.de>
2019-08-25 23:50:43 +0200

    Source correct files

--- rtl/PQVexRiscvArtyS7.tcl
+++ rtl/PQVexRiscvArtyS7.tcl
@@ -1,4 +1,4 @@
 set TOPLEVEL "PQVexRiscvArty"
 set PARTNUMBER "xc7s50csga324-1"
-set SOURCES {"PQVexRiscvArty.v"}
+set SOURCES {"PQVexRiscvArtyS7.v"}
 set CONSTRAINTS {"Arty-S7-50-Rev-B-Master.xdc"}
